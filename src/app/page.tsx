472c2a3a3cc558c8f318e1a5fea210465c230746
Kishore Babu Yalamaddi <kishorebabuyalamaddi@farviewglobal.com>
2025-06-18 11:36:15 +0530

    learn

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen flex justify-center items-center bg-blue-400 text-3xl'>PMS</div>
+  )
+}
+
+export default page
