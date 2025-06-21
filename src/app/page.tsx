2c899806b91b5ace2ebae94ab9eb4d1cffd8afc4
Kishore Babu Yalamaddi <kishorebabuyalamaddi@farviewglobal.com>
2025-06-21 12:54:33 +0530

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='min-h-screen flex justify-center items-center bg-blue-400 text-3xl'>CICD</div>
+    <div className='min-h-screen flex justify-center items-center bg-blue-400 text-3xl'>
+      <div>
+        LEARNING CICD
+      </div>
+    </div>
   )
 }
 
